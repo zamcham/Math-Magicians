ad9ef0502b8cf5dc5eeb2abd8522607b141d8c56
zamcham <cristian@zamcham.com>
2023-05-02 13:39:36 -0400

    Import new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="tc-container">
-      <Quote />
-      <Calculator />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 
