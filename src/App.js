866cc4ea124b72f30d9e45ee5ec8ef25c1ed8126
zamcham <cristian@zamcham.com>
2023-05-02 14:35:21 -0400

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 import Home from './components/Home';
@@ -8,6 +7,20 @@ import Home from './components/Home';
 function App() {
   return (
     <>
+      <nav className="navBar">
+        <span>Math Magicians</span>
+        <ul className="menuItems">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
